6dfdc3dddd97f2f1e91c6143462fc15409a6aa44
Chida <chidatakuya1113@gmail.com>
2014-07-06 11:58:45 +0900

    仕様５

---
+++ mission5.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*仕様５：レシピにIDを振ること*/
+int main(void){
+	int num_id;/*num_idはレシピの識別子*/
+	char filename[20];
+	FILE *fp;
+
+
+	/*説明文
+	  ここでファイル名を入力してもらう
+	 */
+	printf("レシピ名を登録します。出力先を入力してください\n");
+	printf("出力先:");
+	scanf("%s",filename);
+	printf("レシピ名を入力してください。\n");
+
+	/*ファイルオープン
+	  ファイル名を入力する前にオープンするとファイル名が意味不明な文字列になる
+	 */
+	if ((fp = fopen(filename, "w")) == NULL) {
+		printf("file open error!!\n");
+		exit(EXIT_FAILURE);	/* エラーの場合は異常終了する */
+	}
+
+	/*レシピ名を受け付け、そのままファイルに書き込む*/
+	for(num_id=1;num_id<4;num_id++){
+		printf("Recipe%d:",num_id);
+		scanf("%s",filename);
+		fprintf(fp,"%d:%s\n",num_id,filename);
+	}
+
+	/*ファイルクローズ*/
+	fclose(fp);
+	return 0;
+}
